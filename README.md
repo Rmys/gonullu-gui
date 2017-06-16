76b0ee662e86871c5d0dd606aee13d7bb818b5d7
Erdem Ersoy <erdemersoy@live.com>
2017-06-16 18:24:00 +0300

    Implement stub application

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # gonullu-gui
-Graphical user interface (GUI) for Pisi GNU/Linux's gonullu application. For information about gonullu, see gonullu's GitHub repository: https://github.com/PisiLinuxNew/gonullu
+Graphical user interface (GUI) for Pisi GNU/Linux's gonullu application. gonullu-gui has been written with Python 3.x and PyQt 5.x. For information about gonullu, see gonullu's GitHub repository: https://github.com/PisiLinuxNew/gonullu
+
+**NOTE:** gonullu-gui is under development (i.e. non-functional).
+
+# Versioning
+Version numbers consist a date in YYYYMMDD format and a revision number for a day (e.g. 20160616.dev1) indicate under development (non-functional) versions. Functional versions come after under development versions. Functional versions consist the final version number and a RC revision number (e.g. 1.0rc2) indicate preview versions. Revision numbers starts from one. The final version number is 1.0.
+
+# Installation and running
+After downloading or cloning this repository, open a terminal in directory that this repository has been downloaded or cloned into and run that command for installation:
+
+    sudo python3 setup.py install
+
+After installation, you can run that in any directory:
+
+    gonullu-gui
+
+**NOTE:** gonullu-gui requires gonullu and PyQt5 but you must install them manually.
---
+++ setup.py
@@ -0,0 +1,47 @@
+#
+#  Copyright 2017 Erdem Ersoy (erdemersoy@live.com)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+from setuptools import setup
+from os import listdir, system
+
+langs = []
+for file in listdir('langs'):
+    if file.endswith('ts'):
+        system('lrelease-qt5 langs/{}'.format(file))
+        langs.append(('langs/{}'.format(file)).replace('.ts', '.qm'))
+
+# system('pyrcc5 gonullu-gui.qrc -o gonullugui/resource.py')
+
+setup(
+    name="gonullu-gui",
+    version="20170616.dev1",
+    packages=["gonullugui"],
+    scripts=["bin/gonullu-gui"],
+    # install_requires=["gonullu"],
+    include_package_data=True,
+    package_data={
+        "": ["*.md"],
+    },
+    author="Erdem Ersoy",
+    author_email="erdemersoy@live.com",
+    description="Graphical user interface for gonullu.",
+    license="GPLv3",
+    keywords=["PyQt5", "gonullu"],
+    url="https://github.com/eersoy93/gonullu-gui",
+)
